9b2892d04697bfb1127fbbb2755a2c8c848cd322
vihoangson <vihoangson@gmail.com>
2016-01-10 00:04:01 +0700

    commit

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -23,10 +23,19 @@ class HomepageController  extends BaseController
 
     public function send_mail(Request $request){
 		$data = ($request->all());
-		$data["senderName"]
-		$data["senderEmail"]
-		$data["subject"]
-		$data["message"]
+
+		$body = "senderName".$data["senderName"]."<br>".
+		"senderEmail".$data["senderEmail"]."<br>".
+		"subject".$data["subject"]."<br>".
+		"message".$data["message"]."<br>";
+		$subject = 'Email từ My Blog '.date("Y-m-d H:i:s");
+		$data = array(
+			'body' => $body
+			);
+		Mail::send($name_email, $data, function ($message) {
+			$message->from('info@vihoangson.com', 'Vi Hoàng Sơn');
+			$message->to('vihoangson@gmail.com')->subject($subject);
+		});
     	echo "success";
     }
 
